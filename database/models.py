b81a3e220287b2fd29580b159e7d3f883aa37c10
yinghdb <yinghdb@163.com>
2016-05-27 11:20:08 +0800

    add the database table of CapitalAccountInfo SecurityAccountInfo UserTable

--- database/models.py
+++ database/models.py
@@ -1,5 +1,36 @@
+#coding:utf-8
 from __future__ import unicode_literals
 
 from django.db import models
 
 # Create your models here.
+
+class CapitalAccountInfo(models.Model):
+	AccountID = models.CharField()
+	Password = models.CharField()
+	Isfirst = models.BooleanField(default=1)
+	UserTable = models.ForeignKey(UserTable)
+	BuyPassword = models.CharField()
+	SecurityAccount = models.ForeignKey(SecurityAccountInfo)
+	loginPwdWrongNum = models.IntegerField(default=0)
+	transPwdWrongNum = models.IntegerField(default=0)
+	lastTimeTrans = models.DateTimeField()
+	lastTimeLogin = models.DateTimeField()
+	IsTransFreeze = models.BooleanField()
+	IsLoginFreeze = models.BooleanField()
+
+class SecurityAccountInfo(models.Model):
+	SecurityID = models.CharField()
+
+class UserTable(models.Model):
+	UserID = models.CharField()
+	Name = models.CharField()
+	IDcard = models.CharField()
+	Gender = models.IntegerField()
+	Occupation = models.CharField()
+	EduInfo = models.CharField()
+	HomeAddr = models.TextField()
+	Department = models.TextField()
+	Tel = models.CharField()
+	MailAddr = models.CharField()
+	Age = models.IntegerField()
