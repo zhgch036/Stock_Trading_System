a27a00b3ca23a1747d622e73a18dbb34a8709ca4
zgc <zhgch036@163.com>
2016-06-04 12:09:58 +0800

    update 5 interface

--- search/views.py
+++ search/views.py
@@ -1,42 +1,48 @@
 #coding:utf8
 from django.shortcuts import render
-from django.http import HttpResponse,HttpResponseRedirect
-from django.core.urlresolvers import reverse
 from database.models import StockInfo
 from .models import StockHistoryInfo
 from django.db.models import Q
 
 # Create your views here.
-stockinfo = 'abc'
 
 def main(req):
-	stockinfo = req.POST['stockinfo']	
 	return render(req,'search.html')
 
 def refresh_5s(req):
+	stockinfo = req.stockinfo
 	try:
 		x = StockInfo.objects.get(Q(StockID=stockinfo)|Q(StockName=stockinfo))
 	except StockInfo.DoesNotExist:
 		pass
 	else:
-		return render(req,'refresh_5s.html',{'CurrentPrice':x.CurrentPrice})
+		return render(req,'refresh_5s.html',{'StockID':x.StockID,'CurrentPrice':x.CurrentPrice})
 
 def refresh_1min(req):
-	x = StockHistoryInfo.objects.filter(Q(StockID=stockinfo)|Q())	#here need datatime
-	return render(req,'refresh_1min.html',{'history_info':x})
-
-def update_realtime(stockid,currentprice):
+	stockid = req.stockid
+	starttime = req.starttime
+	endtime = req.endtime
 	try:
-		x = StockInfo.objects.get(Q(StockID=stockinfo)|Q(StockName=stockinfo))
+		x = StockHistoryInfo.objects.filter(StockID=stockid,HistoryTime__gte=starttime,HistoryTime__lte=endtime)
 	except StockInfo.DoesNotExist:
 		pass
 	else:
-		return render(req,'refresh_5s.html',{'CurrentPrice':x.CurrentPrice})
-	x = StockInfo.objects.get(StockID=stockid)
-	x.CurrentPrice = currentid
-	x.save()
+		return render(req,'refresh_1min.html',{'history_info':x})
 
-def insert_history(stockid,historytime,highestvalue,lowestvalue):
-	x = StockHistoryInfo(StockID=stockid,HistoryTime=historytime,Highest_value=highestvalue,Lowest_value=lowestvalue)
-	x.save()
+def update_realtime(stockcurrentprices):
+	try:
+		for stockid in stockcurrentprices:
+			x = StockInfo.objects.get(StockID=stockid)
+			x.CurrentPrice = stockcurrentprices[stockid]
+			x.save()
+	except StockInfo.DoesNotExist:
+		pass
 
+def insert_history(stockhistoryinfo):
+	for stockid in stockhistoryinfo:
+		historytime = stockhistoryinfo[stockid][0]
+		highestvalue = stockhistoryinfo[stockid][1]
+		lowestvalue = stockhistoryinfo[stockid][2]
+		x = StockHistoryInfo(StockID=stockid,HistoryTime=historytime,Highest_value=highestvalue,Lowest_value=lowestvalue)
+		x.save()
+	
